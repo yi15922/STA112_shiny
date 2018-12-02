7078c7b3ca6907e726ee506e5b7f69919aa49b9d
Joe Zuo <yuncong.zuo@duke.edu>
2018-12-02 19:22:41 +0000

    part02

--- part-02/app.R
+++ part-02/app.R
@@ -8,60 +8,60 @@ library(shinythemes)
 players1 <- read_csv("data/players1.csv")
 
 # UI ---------------------------------------------------------------------------
-ui <- fluidPage(theme = shinytheme("cerulean"),
-  
-  titlePanel("Players browser"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "y", 
-                  label = "Y-axis:",
-                  choices = c("Position" = "position", 
-                              "Age" = "age", 
-                              "Matches" = "matches", 
-                              "Goals" = "goals",
-                              "Own Goals" = "own_goals",
-                              "Assists" = "assists",
-                              "Yellow Cards" = "yellow_cards",
-                              "Red Cards" = "red_cards",
-                              "Substituted On" = "substituted_on",
-                              "Substituted Off" = "substituted_off",
-                              "Market Value" = "market_value",
-                              "Age Range" = "age_range"), 
-                  selected = "position"),
-      selectInput(inputId = "x", 
-                  label = "X-axis:",
-                  choices = c("Position" = "position", 
-                              "Age" = "age", 
-                              "Matches" = "matches", 
-                              "Goals" = "goals",
-                              "Own Goals" = "own_goals",
-                              "Assists" = "assists",
-                              "Yellow Cards" = "yellow_cards",
-                              "Red Cards" = "red_cards",
-                              "Substituted On" = "substituted_on",
-                              "Substituted Off" = "substituted_off",
-                              "Market Value" = "market_value",
-                              "Age Range" = "age_range"), 
-                  selected = "market_value"),
-      selectInput(inputId = "z", 
-                  label = "Color by:",
-                  choices = c("Position" = "position", 
-                              "Age" = "age", 
-                              "Matches" = "matches", 
-                              "Goals" = "goals",
-                              "Own Goals" = "own_goals",
-                              "Assists" = "assists",
-                              "Yellow Cards" = "yellow_cards",
-                              "Red Cards" = "red_cards",
-                              "Substituted On" = "substituted_on",
-                              "Substituted Off" = "substituted_off",
-                              "Market Value" = "market_value",
-                              "Age Range" = "age_range"), 
-                  selected = "age")),
-    mainPanel(plotOutput(outputId = "scatterplot"))
-    
-  )
+ui <- fluidPage(
+  theme = shinytheme("cerulean")
+        titlePanel("Players browser"),
+                
+        sidebarLayout(
+            sidebarPanel(
+                selectInput(inputId = "y", 
+                        label = "Y-axis:",
+                        choices = c("Position" = "position", 
+                                    "Age" = "age", 
+                                    "Matches" = "matches", 
+                                    "Goals" = "goals",
+                                    "Own Goals" = "own_goals",
+                                    "Assists" = "assists",
+                                    "Yellow Cards" = "yellow_cards",
+                                    "Red Cards" = "red_cards",
+                                    "Substituted On" = "substituted_on",
+                                    "Substituted Off" = "substituted_off",
+                                    "Market Value" = "market_value",
+                                    "Age Range" = "age_range"), 
+                        selected = "position"),
+                selectInput(inputId = "x", 
+                        label = "X-axis:",
+                        choices = c("Position" = "position", 
+                                    "Age" = "age", 
+                                    "Matches" = "matches", 
+                                    "Goals" = "goals",
+                                    "Own Goals" = "own_goals",
+                                    "Assists" = "assists",
+                                    "Yellow Cards" = "yellow_cards",
+                                    "Red Cards" = "red_cards",
+                                    "Substituted On" = "substituted_on",
+                                    "Substituted Off" = "substituted_off",
+                                    "Market Value" = "market_value",
+                                    "Age Range" = "age_range"), 
+                         selected = "market_value"),
+                 selectInput(inputId = "z", 
+                         label = "Color by:",
+                         choices = c("Position" = "position", 
+                                     "Age" = "age", 
+                                     "Matches" = "matches", 
+                                     "Goals" = "goals",
+                                     "Own Goals" = "own_goals",
+                                     "Assists" = "assists",
+                                     "Yellow Cards" = "yellow_cards",
+                                     "Red Cards" = "red_cards",
+                                     "Substituted On" = "substituted_on",
+                                     "Substituted Off" = "substituted_off",
+                                     "Market Value" = "market_value",
+                                     "Age Range" = "age_range"), 
+                           selected = "age")),
+                 mainPanel(plotOutput(outputId = "scatterplot"))
+                  
+           )
 )
 
 
@@ -71,7 +71,7 @@ server <- function(input, output) {
   
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
-    ggplot(data = diamonds, aes_string(x = input$x, y = input$y,
+    ggplot(data = players1, aes_string(x = input$x, y = input$y,
                                        color = input$z)) +
       geom_point() +
       labs(x = toTitleCase(str_replace_all(input$x, "_", " ")),
