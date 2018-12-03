2c3b03c26b99fe00a572cea9c4dde9cc7ce5be51
Yi Chen <yi15922@gmail.com>
2018-12-03 17:11:55 +0000

    Corrected styling

--- part-01/app.R
+++ part-01/app.R
@@ -6,53 +6,66 @@ library(tools)
 
 # UI ---------------------------------------------------------------------------
 ui <- fluidPage(
-
   titlePanel("Diamonds browser"),
-  
+
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "y", 
-                  label = "Y-axis:",
-                  choices = c("Carat" = "carat", 
-                              "Depth" = "depth", 
-                              "Table" = "table", 
-                              "Price" = "price"), 
-                  selected = "carat"),
-      selectInput(inputId = "x", 
-                  label = "X-axis:",
-                  choices = c("Carat" = "carat", 
-                              "Depth" = "depth", 
-                              "Table" = "table", 
-                              "Price" = "price"), 
-                  selected = "depth"),
-      selectInput(inputId = "z", 
-                  label = "Color by:",
-                  choices = c("Cut" = "cut", 
-                              "Color" = "color", 
-                              "Clarity" = "clarity"),
-                  selected = "cut")),
+      selectInput(
+        inputId = "y",
+        label = "Y-axis:",
+        choices = c(
+          "Carat" = "carat",
+          "Depth" = "depth",
+          "Table" = "table",
+          "Price" = "price"
+        ),
+        selected = "carat"
+      ),
+      selectInput(
+        inputId = "x",
+        label = "X-axis:",
+        choices = c(
+          "Carat" = "carat",
+          "Depth" = "depth",
+          "Table" = "table",
+          "Price" = "price"
+        ),
+        selected = "depth"
+      ),
+      selectInput(
+        inputId = "z",
+        label = "Color by:",
+        choices = c(
+          "Cut" = "cut",
+          "Color" = "color",
+          "Clarity" = "clarity"
+        ),
+        selected = "cut"
+      )
+    ),
     mainPanel(plotOutput(outputId = "scatterplot"))
-    
   )
 )
 
 # Server -----------------------------------------------------------------------
 
 server <- function(input, output) {
-  
+
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
-    ggplot(data = diamonds, aes_string(x = input$x, y = input$y,
-                                              color = input$z)) +
+    ggplot(data = diamonds, aes_string(
+      x = input$x, y = input$y,
+      color = input$z
+    )) +
       geom_point() +
-      labs(x = toTitleCase(str_replace_all(input$x, "_", " ")),
-           y = toTitleCase(str_replace_all(input$y, "_", " ")),
-           color = toTitleCase(str_replace_all(input$z, "_", " ")))
+      labs(
+        x = toTitleCase(str_replace_all(input$x, "_", " ")),
+        y = toTitleCase(str_replace_all(input$y, "_", " ")),
+        color = toTitleCase(str_replace_all(input$z, "_", " "))
+      )
   })
-
-    }
+}
 
 # Create Shiny app object ------------------------------------------------------
 
 shinyApp(ui = ui, server = server)
-
--- part-02/app.R
+++ part-02/app.R
@@ -12,74 +12,89 @@ players1 <- read_csv("data/players1.csv")
 # UI ---------------------------------------------------------------------------
 ui <- fluidPage(
   theme = shinytheme("cyborg"),
-        titlePanel("Players browser"),
-        sidebarLayout(
-            sidebarPanel(
-                selectInput(inputId = "y", 
-                        label = "Y-axis:",
-                        choices = c("Position" = "position", 
-                                    "Age" = "age", 
-                                    "Matches" = "matches", 
-                                    "Goals" = "goals",
-                                    "Own Goals" = "own_goals",
-                                    "Assists" = "assists",
-                                    "Yellow Cards" = "yellow_cards",
-                                    "Red Cards" = "red_cards",
-                                    "Substituted On" = "substituted_on",
-                                    "Substituted Off" = "substituted_off",
-                                    "Market Value" = "market_value",
-                                    "Age Range" = "age_range"), 
-                        selected = "position"),
-                selectInput(inputId = "x", 
-                        label = "X-axis:",
-                        choices = c("Position" = "position", 
-                                    "Age" = "age", 
-                                    "Matches" = "matches", 
-                                    "Goals" = "goals",
-                                    "Own Goals" = "own_goals",
-                                    "Assists" = "assists",
-                                    "Yellow Cards" = "yellow_cards",
-                                    "Red Cards" = "red_cards",
-                                    "Substituted On" = "substituted_on",
-                                    "Substituted Off" = "substituted_off",
-                                    "Market Value" = "market_value",
-                                    "Age Range" = "age_range"), 
-                         selected = "market_value"),
-                 selectInput(inputId = "z", 
-                         label = "Color by:",
-                         choices = c("Position" = "position", 
-                                     "Age" = "age", 
-                                     "Matches" = "matches", 
-                                     "Goals" = "goals",
-                                     "Own Goals" = "own_goals",
-                                     "Assists" = "assists",
-                                     "Yellow Cards" = "yellow_cards",
-                                     "Red Cards" = "red_cards",
-                                     "Substituted On" = "substituted_on",
-                                     "Substituted Off" = "substituted_off",
-                                     "Market Value" = "market_value",
-                                     "Age Range" = "age_range"), 
-                           selected = "age")),
-                 mainPanel(plotOutput(outputId = "scatterplot"))
-                  
-           )
+  titlePanel("Players browser"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "y",
+        label = "Y-axis:",
+        choices = c(
+          "Position" = "position",
+          "Age" = "age",
+          "Matches" = "matches",
+          "Goals" = "goals",
+          "Own Goals" = "own_goals",
+          "Assists" = "assists",
+          "Yellow Cards" = "yellow_cards",
+          "Red Cards" = "red_cards",
+          "Substituted On" = "substituted_on",
+          "Substituted Off" = "substituted_off",
+          "Market Value" = "market_value",
+          "Age Range" = "age_range"
+        ),
+        selected = "position"
+      ),
+      selectInput(
+        inputId = "x",
+        label = "X-axis:",
+        choices = c(
+          "Position" = "position",
+          "Age" = "age",
+          "Matches" = "matches",
+          "Goals" = "goals",
+          "Own Goals" = "own_goals",
+          "Assists" = "assists",
+          "Yellow Cards" = "yellow_cards",
+          "Red Cards" = "red_cards",
+          "Substituted On" = "substituted_on",
+          "Substituted Off" = "substituted_off",
+          "Market Value" = "market_value",
+          "Age Range" = "age_range"
+        ),
+        selected = "market_value"
+      ),
+      selectInput(
+        inputId = "z",
+        label = "Color by:",
+        choices = c(
+          "Position" = "position",
+          "Age" = "age",
+          "Matches" = "matches",
+          "Goals" = "goals",
+          "Own Goals" = "own_goals",
+          "Assists" = "assists",
+          "Yellow Cards" = "yellow_cards",
+          "Red Cards" = "red_cards",
+          "Substituted On" = "substituted_on",
+          "Substituted Off" = "substituted_off",
+          "Market Value" = "market_value",
+          "Age Range" = "age_range"
+        ),
+        selected = "age"
+      )
+    ),
+    mainPanel(plotOutput(outputId = "scatterplot"))
+  )
 )
 
 
 # Server -----------------------------------------------------------------------
 
 server <- function(input, output) {
-  
+
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
-    ggplot(data = players1, aes_string(x = input$x, y = input$y,
-                                       color = input$z)) +
+    ggplot(data = players1, aes_string(
+      x = input$x, y = input$y,
+      color = input$z
+    )) +
       geom_point() +
-      labs(x = toTitleCase(str_replace_all(input$x, "_", " ")),
-           y = toTitleCase(str_replace_all(input$y, "_", " ")),
-           color = toTitleCase(str_replace_all(input$z, "_", " ")))
+      labs(
+        x = toTitleCase(str_replace_all(input$x, "_", " ")),
+        y = toTitleCase(str_replace_all(input$y, "_", " ")),
+        color = toTitleCase(str_replace_all(input$z, "_", " "))
+      )
   })
-  
 }
 
 
